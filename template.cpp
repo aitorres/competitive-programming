0ba9c86ee46dbe70a8e0994007d08e1f719f777b
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-26 17:45:06 -0400

    Adds first version of C++ template

---
+++ template.cpp
@@ -0,0 +1,36 @@
+/**
+ * Competitive programming C++ template.
+ * This template will be improved over time.
+ *
+ * @author: Andrés Ignacio Torres
+ *          (@aitorres on Github)
+ *          (@andresitorresm on Twitter)
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <list>
+#include <stack>
+#include <set>
+#include <numeric>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// Code goes here
+	return 0;
+}
