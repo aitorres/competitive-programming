f6ba914119b0efdedf00cd3c1eeb69bc1eb38450
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-11-17 21:23:42 -0400

    Adds shortcuts for reading and printing ints

--- template.cpp
+++ template.cpp
@@ -2,6 +2,11 @@
  * Competitive programming C++ template.
  * This template will be improved over time.
  *
+ * Please, always check this file because it includes the latest
+ * template I use, which might not be the case on solved problem
+ * files if they were added to the repository before a given template 
+ * modification.
+ *
  * @author: Andrés Ignacio Torres
  *          (@aitorres on Github)
  *          (@andresitorresm on Twitter)
@@ -26,6 +31,12 @@
 
 #define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
 #define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define RI(x) scanf("%i", &x)
+#define RII(x, y) scanf("%i %i", &x, &y)
+#define RIII(x, y, z) scanf("%i %i %i", &x, &y, &z)
+#define PI(x) printf("%i\n", x)
+#define PII(x, y) scanf("%i %i\n", x, y)
+#define PIII(x, y, z) scanf("%i %i %i\n", x, y, z)
 
 using namespace std;
 
